dd389e9ef4ca27b2e704b487523526e12f5bb07c
OP <pcakesmmm@gmail.com>
2023-01-09 11:31:24 -0500

    disable insecure config

--- src/main/java/com/fileportal/portal/config/WebConfig.java
+++ src/main/java/com/fileportal/portal/config/WebConfig.java
@@ -1,15 +1,9 @@
 package com.fileportal.portal.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*");
-    }
-}
+@EnableScheduling
+public class WebConfig implements WebMvcConfigurer { }
